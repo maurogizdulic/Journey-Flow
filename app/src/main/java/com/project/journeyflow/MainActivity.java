727dcc73aaafee065f2283e8847f64968c692699
Mauro <mauro.gizdulic@gmail.com>
2024-10-30 17:57:37 +0100

    Created bottom navigation bar on main activity.

--- app/src/main/java/com/project/journeyflow/MainActivity.java
+++ app/src/main/java/com/project/journeyflow/MainActivity.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.project.journeyflow.fragments.HistoryFragment;
+import com.project.journeyflow.fragments.HomeFragment;
+import com.project.journeyflow.fragments.ProfileFragment;
+import com.project.journeyflow.fragments.PublicFragment;
+import com.project.journeyflow.fragments.TrackingFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +20,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Home fragment on every entry in app
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment())
+                .commit();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment;
+
+            if (item.getItemId() == R.id.nav_home){
+                selectedFragment = new HomeFragment();
+            }
+            else if (item.getItemId() == R.id.nav_history){
+                selectedFragment = new HistoryFragment();
+            }
+            else if (item.getItemId() == R.id.nav_public){
+                selectedFragment = new PublicFragment();
+            }
+            else if (item.getItemId() == R.id.nav_profile){
+                selectedFragment = new ProfileFragment();
+            }
+            else /*if (item.getItemId() == R.id.nav_tracking)*/{
+                selectedFragment = new TrackingFragment();
+            }
+
+            // Replace the fragment
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit();
+
+            return true;
         });
+
     }
 }
---
+++ app/src/main/java/com/project/journeyflow/fragments/ProfileFragment.java
@@ -0,0 +1,24 @@
+package com.project.journeyflow.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.project.journeyflow.R;
+
+public class ProfileFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return view;
+    }
+}
